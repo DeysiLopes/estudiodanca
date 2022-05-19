029335db203071f8d89adeb297a124e56d500b3c
DeysiLopes <deysilinconl@gmail.com>
2022-05-18 22:20:54 -0300

    Add attributes in  the app

--- src/main/java/lpoo/estudiodanca/modelo/vo/Estudante.java
+++ src/main/java/lpoo/estudiodanca/modelo/vo/Estudante.java
@@ -1,5 +1,43 @@
 package lpoo.estudiodanca.modelo.vo;
 
-public class Estudante {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "tb_pessoa")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Estudante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo")
+    private Long cod;
+    private String nome;
+
+
+    public Estudante() {
+        this.cod = cod;
+    }
+   
+    private String getNome(){
+        return nome;
+    }
+    private String setNome(){
+        this.nome = nome;
+    }
+
+    public Long getCod() {
+        return cod;
+    }
+
+    public void setCod(Long cod) {
+        this.cod = cod;
+    }
 }
