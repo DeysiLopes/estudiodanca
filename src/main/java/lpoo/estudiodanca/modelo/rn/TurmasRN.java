e9a6bc85716e1dfb8a5525de4082230a24d5b485
DeysiLopes <deysilinconl@gmail.com>
2022-05-18 22:13:17 -0300

    Create Turmas in the app

---
+++ src/main/java/lpoo/estudiodanca/modelo/rn/TurmasRN.java
@@ -0,0 +1,61 @@
+package com.mycompany.lpoofinal.modelo.rn;
+
+import java.util.List;
+
+import lpoo.estudiodanca.modelo.dao.GenericDao;
+
+public class Turmas {
+    private GenericDao<Turma> genericDao;
+
+    public TurmaRN() {
+        genericDao = new GenericDao<Turma>();
+    }
+
+    public void gravar(Turma pro) {
+        genericDao.save(pro);
+    }
+
+    public void atualizar(Turma pro) {
+        genericDao.update(pro);
+    }
+
+    public void remover(Turma pro) {
+        genericDao.delete(pro);
+    }
+
+    //ARRUMAR
+    public void remover(Turma pro, List<Contato> contatos) {
+        genericDao.getManager().getTransaction().begin();
+
+        contatos.forEach(contato -> {
+            genericDao.getManager().remove(contato);
+        });
+
+        genericDao.getManager().remove(pro);
+        genericDao.getManager().getTransaction().commit();
+//        System.out.println(contato.getClass().getSimpleName() + " excluído com sucesso!");
+        System.out.println(pro.getClass().getSimpleName() + " excluído com sucesso!");
+    }
+
+    public List buscarTodos() {
+        List<Turma> Turmas = genericDao.listAll(Turma.class);
+
+        return Turmas;
+    }
+
+    public List buscar(String nome, String nomePessoa) {
+        try {
+            return genericDao.search(nome, nomePessoa, Turma.class);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Funcionario buscar(Long id) {
+        try {
+            return genericDao.search(Turma.class, id);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
