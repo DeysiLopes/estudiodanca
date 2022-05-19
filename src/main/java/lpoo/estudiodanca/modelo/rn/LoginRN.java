6ca9f725f782256407c362a5b8f8853821be85cf
DeysiLopes <deysilinconl@gmail.com>
2022-05-18 22:13:42 -0300

    Create LoginRN in the app

---
+++ src/main/java/lpoo/estudiodanca/modelo/rn/LoginRN.java
@@ -0,0 +1,38 @@
+package com.mycompany.lpoofinal.modelo.rn;
+
+import java.util.List;
+
+import lpoo.estudiodanca.modelo.dao.GenericDao;
+
+public class LoginRN {
+    
+    private GenericDao<Login> genericDao;
+
+    public LoginRN() {
+        this.genericDao = new GenericDao<Login>();
+    }
+
+    public void gravar(Login login) {
+        genericDao.save(login);
+    }
+    
+    public void atualizar(Login login) {
+        genericDao.update(login);
+    }
+
+    public void remover(Login login) {
+        genericDao.delete(login);
+    }
+
+    public List buscarTodos() {
+        return genericDao.listAll(Login.class);
+    }
+
+    public Login buscar(String login) {
+        try {
+            return genericDao.listOne("usuario", login, Login.class);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
